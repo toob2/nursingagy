2b2be0ecbe3be1797da149104bcb1f91be0beffa
Tomas Obdrzalek <obdrzalek.tom@gmail.com>
2018-06-01 08:39:27 +0100

    navbeta

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,9 +1,11 @@
 const toggler = document.querySelector('.nav__toggler');
 const navList = document.querySelector('.nav__list');
+const span  = document.querySelector('span');
 
 function navToggle(){
   navList.classList.toggle('nav__list');
   navList.classList.toggle('nav__list--mobile');
+  span.classList.toggle('active');
 };
 
 toggler.addEventListener('click', navToggle);
