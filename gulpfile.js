125de6b4579cb7e77945d392fa0dcbb8998974d2
Tomas Obdrzalek <obdrzalek.tom@gmail.com>
2018-06-05 00:53:37 +0100

    vh

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass');
 gulp.task('sass', function(){
   gulp.src('app/scss/**/*.scss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('dist/css/'));
+  .pipe(gulp.dest('app/css/'));
 });
 
 gulp.watch('app/scss/**/*.scss', ['sass']);
